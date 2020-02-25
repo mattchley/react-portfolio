06e50f7c653b0a6147d9aa4274eae79b78abc3e0
mattchley <mattchley@gmail.com>
2020-02-25 12:27:50 -0800

    SketchContactPage

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,75 +1,45 @@
-import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import React from "react";
 import { Col, Row, Container } from "../components/Grid";
-import Jumbotron from "../components/Jumbotron";
-import API from "../utils/API";
+import Title from "../components/Title"
+import { Input, TextArea, FormBtn } from "../components/Form";
+
 
 function Contact(props) {
-  const [book, setBook] = useState({})
 
-  // When this component mounts, grab the book with the _id of props.match.params.id
-  // e.g. localhost:3000/books/599dcb67f0f16317844583fc
-  const {id} = useParams()
-  useEffect(() => {
-    API.getBook(id)
-      .then(res => setBook(res.data))
-      .catch(err => console.log(err));
-  }, [])
 
   return (
-      <Container fluid>
-        <Row>
-          <Col size="md-12">
-            <Jumbotron>
-              <h1>
-                {book.title} by {book.author}
-              </h1>
-            </Jumbotron>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-10 md-offset-1">
-            <article>
-              <h1>Synopsis</h1>
-              <p>
-                {book.synopsis}
-              </p>
-            </article>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-2">
-            <Link to="/">← Back to Authors</Link>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+    <Container fluid>
+      <Row>
+        <Col size="lg-12">
+          <Title />
+          <form>
+            <Input
+
+              name="Email"
+              placeholder="name@example.com (required)"
+            />
+            <Input
+
+              name="name"
+              placeholder="Taylor Doe (required)"
+            />
+            <TextArea
+
+              name="message"
+              placeholder="Your message here ..."
+            />
+            <FormBtn
+              // disabled={!(formObject.author && formObject.title)}
+              // onClick={handleFormSubmit}
+            >
+              Submit
+</FormBtn>
+          </form>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
 
 
 export default Contact;
-
-
-{/* <form>
-<Input
-  onChange={handleInputChange}
-  name="title"
-  placeholder="Title (required)"
-/>
-<Input
-  onChange={handleInputChange}
-  name="author"
-  placeholder="Author (required)"
-/>
-<TextArea
-  onChange={handleInputChange}
-  name="synopsis"
-  placeholder="Synopsis (Optional)"
-/>
-<FormBtn
-  disabled={!(formObject.author && formObject.title)}
-  onClick={handleFormSubmit}
->
-  Submit Book
-</FormBtn>
-</form> */}
--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { Col, Row, Container } from "../components/Grid";
-import Jumbotron from "../components/Jumbotron";
+import Title from "../components/Title"
+import Content from "../components/Content"
+import Image from "../components/Image"
+import Links from "../components/Links"
 
 function Portfolio() {
   return (
     <Container fluid>
       <Row>
-        <Col size="md-12">
-          <Jumbotron>
-            <h1>404 Page Not Found</h1>
-            <h1>
-              <span role="img" aria-label="Face With Rolling Eyes Emoji">
-                🙄
-              </span>
-            </h1>
-          </Jumbotron>
+        <Col size="md-6">
+          <Title />
+          <Col size="md-2">
+          <Image/>
+          </Col>
+          <Col size="md-10">
+          <Content/>
+          <Links/>
+          </Col>
         </Col>
       </Row>
     </Container>
