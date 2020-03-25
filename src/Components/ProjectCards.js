ba4a17e054bc7ba33c8776ca4461d75698c9c52a
mattchley <mattchley@gmail.com>
2020-03-25 13:01:27 -0700

    basicFormating

--- src/Components/ProjectCards.js
+++ src/Components/ProjectCards.js
@@ -5,17 +5,50 @@ import Druggler from "../Images/ProjectPics/Druggler.png";
 import MovieMeme from "../Images/ProjectPics/Movie-Meme-Mash.png";
 import WrkOut from "../Images/ProjectPics/WrkOutTrkrOFF.png";
 import BdgtTrker from "../Images/ProjectPics/BudgetTrckrOFF.png";
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 function ProjectCard() {
     return (
-        <div>
-            <img src={EmployDB}></img>
-            <img src={SocialEarPiece}></img>
-            <img src={Druggler}></img>
-            <img src={MovieMeme}></img>
-            <img src={WrkOut}></img>
-            <img src={BdgtTrker}></img>
-        </div>
+        <Container maxWidth="lg">
+            <Grid container spacing={1}>
+                <Grid item xs={12} sm={6}>
+                    <Paper >
+                        <h1>Node.js SQL EmployeeDB</h1>
+                        <img src={EmployDB} style={{ maxHeight: "340px", maxWidth: "300px" }}></img>
+                        <p>Description of the project goes here!</p>
+                    </Paper>
+                    <Paper >
+                        <h1>Social Ear SocialEarPiece</h1>
+                        <img src={SocialEarPiece} style={{ maxHeight: "340px", maxWidth: "300px" }}></img>
+                        <p>Description of the project goes here!</p>
+                    </Paper>
+                    <Paper >
+                        <h1>Druggle</h1>
+                        <img src={Druggler} style={{ maxHeight: "340px", maxWidth: "300px" }}></img>
+                        <p>Description of the project goes here!</p>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <Paper >
+                        <h1>Movie Meme Mash</h1>
+                        <img src={MovieMeme} style={{ maxHeight: "340px", maxWidth: "300px" }}></img>
+                        <p>Description of the project goes here!</p>
+                    </Paper>
+                    <Paper >
+                        <h1>MongoDB PWA Work Out Tracker</h1>
+                        <img src={WrkOut} style={{ maxHeight: "340px", maxWidth: "300px" }}></img>
+                        <p>Description of the project goes here!</p>
+                    </Paper>
+                    <Paper >
+                        <h1>PWA Budget Tracker</h1>
+                        <img src={BdgtTrker} style={{ maxHeight: "340px", maxWidth: "300px" }}></img>
+                        <p>Description of the project goes here!</p>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </Container>
     )
 }
 
