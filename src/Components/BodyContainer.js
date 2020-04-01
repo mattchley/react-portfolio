2e33984abaabde6a26d544b51d324896fc1c2ff7
mattchley <mattchley@gmail.com>
2020-04-01 13:08:15 -0700

    heroku build

--- src/Components/BodyContainer.js
+++ src/Components/BodyContainer.js
@@ -15,6 +15,7 @@ function BodyContainer() {
                 </Grid>
                 <Grid item xs={12} md={9} style={{ backgroundColor: "lightblue" }}>
                     <p>Front End Developer with a background in Anthropology and Customer Service wanting to create a more informed and accommodating UI for the web. Recently secured a certificate for Full Stack Development from UCSD, with the skills to work with JavaScript, React.js, CSS, MongoDB and SQL. Have several projects under my belt; Ranging from simple employee DBs to a group application that blended drug interactions with timer for when they take their prescriptions, with several more in the pipeline. Going into every project, I want the client to get the information they desire along with a little more that will send them out with a better sense of understanding. I am passionate about growing my skill set to allow me to connect with the client in a way they never thought they wanted.</p>
+                    {/* Make this a modal or link to the contact page? */}
                     <h2>Email: mattchley@gmail.com</h2>
                     <Grid>
                         <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
