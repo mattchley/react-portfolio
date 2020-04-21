0e5a7e68fb9a83694b592b3e7b286ca80f6415d0
mattchley <mattchley@gmail.com>
2020-04-21 13:21:08 -0700

    form functionality and state

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -1,19 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
 function ContactForm() {
+
+    const [email, setEmail] = useState();
+    const [name, setName] = useState();
+    const [comment, setComment] = useState();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email, name, comment)
+        
+        const resetForm =() => {
+            setEmail('')
+            setName('')
+            setComment('')
+
+        }
+        resetForm()
+    }
+
     return (
         <Container maxWidth="lg">
             <form noValidate autoComplete="off">
                 <h4>Email Address</h4>
-                <TextField id="filled-basic" label="name@example.com" variant="filled" fullWidth="true" />
+                <TextField
+                    type='text'
+                    id="filled-basic"
+                    label="name@example.com"
+                    variant="filled"
+                    fullWidth="true"
+                    value={email}
+                    onChange={e => setEmail(e.target.value.trim())} />
                 <h4>Name</h4>
-                <TextField id="filled-basic" label="Jordan Rogers" variant="filled" fullWidth="true" />
+                <TextField
+                    type='text'
+                    id="filled-basic"
+                    label="Jordan Rogers"
+                    variant="filled"
+                    fullWidth="true"
+                    value={name}
+                    onChange={e => setName(e.target.value.trim())} />
                 <h4>Comment</h4>
-                <TextField id="filled-basic" label="Your message here..." variant="filled" fullWidth="true" />
-                <Button color="primary" variant="contained">SUBMIT</Button>
+                <TextField
+                    type='text'
+                    id="filled-basic"
+                    label="Your message here..."
+                    variant="filled"
+                    fullWidth="true"
+                    value={comment}
+                    onChange={e => setComment(e.target.value.trim())} />
+                <Button color="primary" variant="contained" onClick={handleSubmit}>SUBMIT</Button>
             </form>
         </Container>
     )
