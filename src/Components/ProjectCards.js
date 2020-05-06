1dfada621c561bf35a91f33397a6e136163188a1
mattchley <mattchley@gmail.com>
2020-05-06 11:11:03 -0700

    centered link images

--- src/Components/ProjectCards.js
+++ src/Components/ProjectCards.js
@@ -16,13 +16,15 @@ function ProjectCard() {
             <Grid container spacing={1} justify={'center'}>
                 <Grid item xs={12} sm={8}>
                     <Grid>
-                        <Paper >
+                        <Paper>
                             <h2>Druggler</h2>
                             <img src={Druggler} alt="Druggler" style={{ maxHeight: "340px" }} ></img>
                             <br />
                             <p>Tracks drug interactions and times to take medication.</p>
-                            <a href="https://github.com/mattchley/Druggler"><img src="https://img.icons8.com/ios-glyphs/60/000000/github.png" /></a>
-                            <a href="https://druggler.herokuapp.com/"><img src="https://img.icons8.com/ios/50/000000/heroku.png" /></a>
+                            <Grid container spacing={1} justify={'center'}>
+                                <a href="https://github.com/mattchley/Druggler"><img src="https://img.icons8.com/ios-glyphs/60/000000/github.png" /></a>
+                                <a href="https://druggler.herokuapp.com/"><img src="https://img.icons8.com/ios/50/000000/heroku.png" /></a>
+                            </Grid>
                         </Paper>
                     </Grid>
                     <Grid>
@@ -31,8 +33,10 @@ function ProjectCard() {
                             <img src={MovieMeme} alt="Movie Meme Mash" style={{ maxHeight: "340px" }} ></img>
                             <br />
                             <p>Simple movie review app where the client logs in and leaves a comment and gif as a review.</p>
-                            <a href="https://github.com/mattchley/Movie-Meme-Mash"><img src="https://img.icons8.com/ios-glyphs/60/000000/github.png" /></a>
-                            <a href="https://test-memms.herokuapp.com/"><img src="https://img.icons8.com/ios/50/000000/heroku.png" /></a>
+                            <Grid container spacing={1} justify={'center'}>
+                                <a href="https://github.com/mattchley/Movie-Meme-Mash"><img src="https://img.icons8.com/ios-glyphs/60/000000/github.png" /></a>
+                                <a href="https://test-memms.herokuapp.com/"><img src="https://img.icons8.com/ios/50/000000/heroku.png" /></a>
+                            </Grid>
                         </Paper>
                     </Grid>
                     <Grid>
@@ -41,7 +45,9 @@ function ProjectCard() {
                             <img src={EmployDB} alt="Node.js SQL EmployeeDB" style={{ maxHeight: "340px" }} ></img>
                             <br />
                             <p>Node Js and SQL app that tracks employees by department.</p>
-                            <a href="https://github.com/mattchley/Employee_Summary"><img src="https://img.icons8.com/ios-glyphs/60/000000/github.png" /></a>
+                            <Grid container spacing={1} justify={'center'}>
+                                <a href="https://github.com/mattchley/Employee_Summary"><img src="https://img.icons8.com/ios-glyphs/60/000000/github.png" /></a>
+                            </Grid>
                         </Paper>
                     </Grid>
                     <Grid>
@@ -50,8 +56,10 @@ function ProjectCard() {
                             <img src={SocialEarPiece} alt="Social Ear Piece" style={{ maxHeight: "340px" }} ></img>
                             <br />
                             <p>Supply of funny lines and interesting quotes.</p>
-                            <a href="https://github.com/Louis100X/Social_Ear_Whisper"><img src="https://img.icons8.com/ios-glyphs/60/000000/github.png" /></a>
-                            <a href="https://social-ear-whisper.herokuapp.com/"><img src="https://img.icons8.com/ios/50/000000/heroku.png" /></a>
+                            <Grid container spacing={1} justify={'center'}>
+                                <a href="https://github.com/Louis100X/Social_Ear_Whisper"><img src="https://img.icons8.com/ios-glyphs/60/000000/github.png" /></a>
+                                <a href="https://social-ear-whisper.herokuapp.com/"><img src="https://img.icons8.com/ios/50/000000/heroku.png" /></a>
+                            </Grid>
                         </Paper>
                     </Grid>
                     <Grid>
@@ -60,8 +68,10 @@ function ProjectCard() {
                             <img src={WrkOut} alt="WrkOutTrkr" style={{ maxHeight: "340px" }} ></img>
                             <br />
                             <p>MongoDB based work out tracker.</p>
-                            <a href="https://github.com/mattchley/WrkOutTrkr"><img src="https://img.icons8.com/ios-glyphs/60/000000/github.png" /></a>
-                            <a href="https://wrkouttrkr.herokuapp.com/"><img src="https://img.icons8.com/ios/50/000000/heroku.png" /></a>
+                            <Grid container spacing={1} justify={'center'}>
+                                <a href="https://github.com/mattchley/WrkOutTrkr"><img src="https://img.icons8.com/ios-glyphs/60/000000/github.png" /></a>
+                                <a href="https://wrkouttrkr.herokuapp.com/"><img src="https://img.icons8.com/ios/50/000000/heroku.png" /></a>
+                            </Grid>
                         </Paper>
                     </Grid>
                     <Grid>
@@ -70,8 +80,11 @@ function ProjectCard() {
                             <img src={BdgtTrker} alt="Node.js GitHub Profile Generator" style={{ maxHeight: "340px", maxWidth: "300px" }} ></img>
                             <br />
                             <p>Simple PDF Generator based on GitHub user data</p>
-                            <a href="https://github.com/mattchley/GitHubeNodeProfile"><img src="https://img.icons8.com/ios-glyphs/60/000000/github.png" /></a>
+                            <Grid container spacing={1} justify={'center'}>
 
+                                <a href="https://github.com/mattchley/GitHubeNodeProfile"><img src="https://img.icons8.com/ios-glyphs/60/000000/github.png" /></a>
+
+                            </Grid>
                         </Paper>
                     </Grid>
                 </Grid>
